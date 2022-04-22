921a7cf7c761e579ee61fb9d99925ee597d25570
Michael Owuor <michael.owuor@gmail.com>
2022-04-22 09:59:22 +0300

    styles: modified global styles.

--- utilities/GraphQLQuery.js
+++ utilities/GraphQLQuery.js
@@ -4,10 +4,10 @@ import { useQuery } from '@apollo/client';
 const Query = ({ children, query, slug }) => {
 	const { data, loading, error } = useQuery(query, { variables: { slug: slug } });
 
-	if (loading) return <p className="center-text-screen">loading...</p>;
+	if (loading) return <p className="center-on-screen">loading...</p>;
 
 	if (error)
-		return <p className="center-text-screen text-red-500">Error: {error.message}</p>;
+		return <p className="center-on-screen text-red-500">Error: {error.message}</p>;
 
 	return children({ data });
 };
